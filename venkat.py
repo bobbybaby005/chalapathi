b3479f038965ad37d7e210c4b7eadeea2052ae0d
Chalapathi Rao Vaddi <vaddi.bobby63@gmail.com>
2022-02-17 18:28:23 +0530

    familylove

--- venkat.py
+++ venkat.py
@@ -1,10 +1,24 @@
 
 love you daddy
 
+<<<<<<< HEAD
 love you amma
 
 love you sailu
 
 love you chinni
+=======
+love you baby
+
+love you udhu
+
+love you sai
+
+love you tanvith
+>>>>>>> sprint1
+
+love you ramu bava
+
+love you narendra bava
 
 
